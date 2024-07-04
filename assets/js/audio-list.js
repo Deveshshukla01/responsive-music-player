666d16ccbd722c226d744dc803330b2cfcf2fa5f
Deveshshukla01 <157008187+Deveshshukla01@users.noreply.github.com>
2024-07-04 20:55:43 +0530

    Add files via upload

---
+++ assets/js/audio-list.js
@@ -0,0 +1,38 @@
+let allMusic =[
+{
+    name:"happier",
+    artist:"Billie Eilish",
+    img: "music-1",
+    src:"music-1" ,
+},
+{
+    name:"kammo ji ",
+    artist:"Talwiinder",
+    img: "music-2",
+    src:"music-2" ,
+},
+{
+    name:"tum jo aaye ",
+    artist:"rahat fateh ali khan",
+    img: "music-3",
+    src: "music-3",
+},
+{
+    name:"jiya dhadak dhadak",
+    artist:"rajat fateh ali khan",
+    img: "music-4",
+    src: "music-4",
+},
+{
+    name:"pehli nazar mein",
+    artist:"atif Aslam",
+    img:"music-5" ,
+    src:"music-5" ,
+},
+{
+    name:"video games",
+    artist:"Lana Del Rey",
+    img:"music-6" ,
+    src: "music-6",
+},
+];
